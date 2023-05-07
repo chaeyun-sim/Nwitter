fda65e2decd1c45d3b2f0c11c2f57507d315a7ab
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-05-08 02:06:25 +0900

    feat: add mock data about avatars and trends

---
+++ src/pages/HomePage/avatarData.js
@@ -0,0 +1,17 @@
+export const avatars = [
+  {
+    image: 'https://cdn.britannica.com/05/236505-050-17B6E34A/Elon-Musk-2022.jpg',
+    name: 'Ilon Musk',
+    id: '@elonmusk'
+  },
+  {
+    image: 'https://i0.wp.com/short-biography.com/wp-content/uploads/rihanna/Rihanna.jpg?w=960&ssl=1',
+    name: 'Rihanna',
+    id: '@rihanna'
+  },
+  {
+    image: 'https://gracemcgettigan.files.wordpress.com/2015/01/tay.jpg',
+    name: 'Taylor Swift',
+    id: '@taylorswift13'
+  }
+]
---
+++ src/pages/HomePage/trendData.js
@@ -0,0 +1,46 @@
+export const data = [
+  {
+    title: '#Javascript',
+    count: '58.7K Nweets'
+  },
+  {
+    title: '#React',
+    count: '5,907 Nweets'
+  },
+  {
+    title: 'Happy Coding With',
+    count: ''
+  },
+  {
+    title: '#CSS',
+    count: '2,593 Nweets'
+  },
+  {
+    title: '#FrontEnd',
+    count: '19.6K Nweets'
+  },
+  {
+    title: "It dosen't work",
+    count: ''
+  },
+  {
+    title: '#npm',
+    count: '1,729 Nweets'
+  },
+  {
+    title: '#FML',
+    count: '59.6K Nweets'
+  },
+  {
+    title: 'Digital Nomad',
+    count: '11.1K Nweets'
+  },
+  {
+    title: 'Digital Nomad',
+    count: '11.1K Nweets'
+  },
+  {
+    title: 'Why does it',
+    count: ''
+  }
+]
